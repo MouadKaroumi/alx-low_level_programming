7a9b4604334cd5d66113e52945c2b8a2dbb502f1
Mouad <karoumimouad@gmail.com>
2023-02-16 12:16:47 -0800

    task5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
  *    * Description: prints "Programming is like building a multilingual puzzle"
  *     *
  *      * Return: Always 0 (Success)
- *       */
-
+ *      
+  */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
